27d2e6b1ae66eeff4e3257bb31e0e99e860463e7
bluepnwage <a.carty2555@gmail.com>
2022-11-07 18:01:09 -0400

    improve demo sidebar

--- components/demo/Sidebar.tsx
+++ components/demo/Sidebar.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { cx } from "@util/cx";
 import Link from "next/link";
-import { useSelectedLayoutSegments } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 export function Sidebar() {
-  const [segment] = useSelectedLayoutSegments();
+  const path = usePathname();
   return (
     <div
       style={{ height: "calc(100vh - 48px)" }}
@@ -13,13 +13,16 @@ export function Sidebar() {
       <p className="text-gray-500 text-xl font-semibold mb-2">Core Features</p>
       <ul className="text-center space-y-2 w-full mb-5">
         {coreFeatures.map((route, key) => {
-          const active = route.href === segment || (segment === null && key === 0);
+          const active = path === route.path;
           return (
-            <li key={key}>
+            <li key={key} className="mx-auto inline-block hover:bg-gray-100 duration-200 ease-out w-4/5 rounded-md">
               <Link
                 aria-current={active ? "page" : "false"}
-                className={cx(active ? "font-semibold" : "", "text-gray-500 text-center")}
-                href={`/demo/${route.href}`}
+                className={cx(
+                  active ? "font-semibold" : "",
+                  "text-gray-500 text-center w-full h-full py-2 inline-block"
+                )}
+                href={route.path}
               >
                 {route.name}
               </Link>
@@ -28,18 +31,20 @@ export function Sidebar() {
         })}
       </ul>
       <p className="text-gray-500 text-xl font-semibold mb-2">Data Fetching</p>
-      <ul className="space-y-2 w-full text-gray-500 ">
-        {demoRoutes.map((link, key) => {
-          console.log(link.href, segment);
-          const active = link.href === segment || (segment === null && key === 0);
+      <ul className="space-y-2 w-full text-gray-500 text-center ">
+        {demoRoutes.map((route, key) => {
+          const active = path === route.path;
           return (
-            <li key={key} className="text-center">
+            <li key={key} className="mx-auto inline-block hover:bg-gray-100 duration-200 ease-out w-4/5 rounded-md">
               <Link
                 aria-current={active ? "page" : "false"}
-                className={cx(active ? "font-semibold" : "", "text-gray-500  text-center")}
-                href={`/demo/data-fetching/${link.href}`}
+                className={cx(
+                  active ? "font-semibold" : "",
+                  "text-gray-500 text-center w-full h-full py-2 inline-block"
+                )}
+                href={route.path}
               >
-                {link.name}
+                {route.name}
               </Link>
             </li>
           );
@@ -52,37 +57,37 @@ export function Sidebar() {
 const coreFeatures = [
   {
     name: "Overview",
-    href: ""
+    path: "/demo"
   },
   {
     name: "API Routes",
-    href: "api-routes"
+    path: "/demo/api-routes"
   },
   {
     name: "Client-side routing",
-    href: "client-side-routing"
+    path: "/demo/client-side-routing"
   },
   {
     name: "Performance",
-    href: "performance"
+    path: "/demo/performance"
   }
 ];
 
 const demoRoutes = [
   {
     name: "Overview",
-    href: ""
+    path: "/demo/data-fetching"
   },
   {
     name: "Static-Site Generation",
-    href: "ssg"
+    path: "/demo/data-fetching/ssg"
   },
   {
     name: "Server-Side Rendering",
-    href: "ssr"
+    path: "/demo/data-fetching/ssr"
   },
   {
     name: "Suspense/Streaming",
-    href: "streaming"
+    path: "/demo/data-fetching/streaming"
   }
 ];
