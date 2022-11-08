41dd4c9dc452edbd2857801af88f2f2536babdf7
bluepnwage <a.carty2555@gmail.com>
2022-11-08 10:19:42 -0400

    fix esLint error in footer

--- components/demo/Footer.tsx
+++ components/demo/Footer.tsx
@@ -5,17 +5,28 @@ export function Footer() {
     <footer className="py-5 border-t w-4/5 space-y-3 text-gray-600 text-center bg-white">
       <p>
         Demo brought to you by{" "}
-        <a href={"https://bluepnwage-portfolio.vercel.app"} target={"_blank"} className="text-indigo-600 ">
+        <a
+          rel={"noreferrer"}
+          href={"https://bluepnwage-portfolio.vercel.app"}
+          target={"_blank"}
+          className="text-indigo-600 "
+        >
           Agis Carty
         </a>
       </p>
       <p>
         View the source code{" "}
-        <a href={"#"} target={"_blank"} className="text-indigo-600">
+        <a rel={"noreferrer"} href={"#"} target={"_blank"} className="text-indigo-600">
           Here
         </a>
       </p>
-      <a aria-label="Github profile" href={"https://github.com/bluepnwage"} target={"_blank"} className="inline-block">
+      <a
+        rel={"noreferrer"}
+        aria-label="Github profile"
+        href={"https://github.com/bluepnwage"}
+        target={"_blank"}
+        className="inline-block"
+      >
         <IconBrandGithub />
       </a>
     </footer>
