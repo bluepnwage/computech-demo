843dc37aef888be01314845496af4840da44260c
bluepnwage <a.carty2555@gmail.com>
2022-11-07 20:40:36 -0400

    adjust head for demo segment

---
+++ app/demo/head.tsx
@@ -0,0 +1,7 @@
+export default function DemoHead() {
+  return (
+    <>
+      <title>Computech | Demo</title>
+    </>
+  );
+}
--- components/demo/NestedNav.tsx
+++ components/demo/NestedNav.tsx
@@ -1,7 +1,7 @@
-// "use client";
+"use client";
 import Link from "next/link";
-// import { useSelectedLayoutSegments } from "next/navigation";
-import { cx } from "../../util/cx";
+import { useSelectedLayoutSegments } from "next/navigation";
+import { cx } from "@util/cx";
 const blogs = [{ id: "1" }, { id: "2" }, { id: "3" }];
 
 interface PropTypes {
@@ -9,24 +9,21 @@ interface PropTypes {
 }
 
 export function NestedNav({ slug }: PropTypes) {
-  // const [segments] = useSelectedLayoutSegments();
+  const [segments] = useSelectedLayoutSegments();
   return (
     <div className="border-b p-2 flex justify-center -mx-4 -mt-4 mb-4">
       <ul className="flex gap-5 mx-auto">
         <li>
-          <Link
-            // className={!segments ? "text-indigo-600" : "text-gray-900"}
-            href={`/demo/data-fetching/${slug}`}
-          >
+          <Link className={!segments ? "text-indigo-600" : "text-gray-900"} href={`/demo/data-fetching/${slug}`}>
             Home
           </Link>
         </li>
         {blogs.map((blog, key) => {
-          // const active = blog.id === segments;
+          const active = blog.id === segments;
           return (
             <li key={key}>
               <Link
-                // className={cx(active ? "text-indigo-600" : "text-gray-900")}
+                className={cx(active ? "text-indigo-600" : "text-gray-900")}
                 href={`/demo/data-fetching/${slug}/${blog.id}`}
               >{`Blog #${blog.id}`}</Link>
             </li>
